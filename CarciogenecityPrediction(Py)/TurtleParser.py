71f0356d6453b144cac12dbcd087f0b2bcf6c668
AbhratanuSurai <abhratanu.surai@gmail.com>
2021-07-03 10:04:18 +0200

    getting two lists for positive and negative examples by parsing the turtle file using rdflib

--- CarciogenecityPrediction(Py)/TurtleParser.py
+++ CarciogenecityPrediction(Py)/TurtleParser.py
@@ -3,47 +3,48 @@ from rdflib import Graph
 
 subjects = set()
 predicates = set()
-objects = set()
 eachPositiveLabels = []
 eachNegativeLabels = []
 allPositiveLabels = []
 allNegativeLabels = []
 
+# Initializing a graph and parsing a turtle file in graph structure
 
 g = Graph()
 result = g.parse("kg-mini-project-train_v2.ttl", format="ttl")
 
-# print out all the triples in the graph
-# print(len(g))
+# Storing distinct subjects and distinct predicates
+# in the sets subjects and predicates
+
 for s in g.subjects():
     subjects.add(s)
 for p in g.predicates():
     predicates.add(p)
-# print(subjects)
-# print(predicates)
 
-# for s in subjects:
-#     for p in predicates:
-#         for o in g.objects(s, p):
-#             objects.add(o)
-# print(objects)
-
-# for triple in g.triples( (rdflib.term.URIRef('https://lpbenchgen.org/resource/lp_4'),
-#                           rdflib.term.URIRef('https://lpbenchgen.org/property/includesResource'),
-#                           None) ):
-#     print(triple)
+# eachPositiveLabels is a list of positive examples
+#     that contains 2 rdflib.term.URIRef items;
+#     1) Name of the learning problem
+#     2) The carcinogenesis bond itself
+#
+#     allPositiveLabels is a list of lists of above type
+#
+# Similar lists also exists for negative examples as well
+# namely eachLearningProblemNegativeLabels and allLearningProblemPositiveLabels.
 
 for s in list(subjects):
     for p in list(predicates):
         if str(p) == "https://lpbenchgen.org/property/includesResource":
             for o in g.objects(s, p):
-                eachPositiveLabels = [s, p, o]
+                eachPositiveLabels = [s, o]
                 allPositiveLabels.append(eachPositiveLabels)
         elif str(p) == "https://lpbenchgen.org/property/excludesResource":
             for o in g.objects(s, p):
-                eachNegativeLabels = [s, p, o]
+                eachNegativeLabels = [s, o]
                 allNegativeLabels.append(eachNegativeLabels)
-print(len(allPositiveLabels))
-print(len(allNegativeLabels))
+
+print("===== All positive examples ===== \n")
+print(allPositiveLabels)
+print("\n===== All Negative examples ===== \n")
+print(allNegativeLabels)
 
 
