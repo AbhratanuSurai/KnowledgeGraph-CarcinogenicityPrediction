3a08af335dbd1c8fefc4127ec544b7a05e16974f
DobinFubiss <simonkuerten97@gmail.com>
2021-07-06 11:44:20 +0200

    test

--- CarciogenecityPrediction(Py)/test.py
+++ CarciogenecityPrediction(Py)/test.py
@@ -1,8 +1,14 @@
+from rdflib import Graph
 from TurtleParser import TurtleParser
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     test = TurtleParser()
     test.parse_rdf()
     bla = test.get_subjects().pop()
     print(bla)
-    print(test.get_labels(bla, 1))
+    data_pos = test.get_labels(bla, 1)
+    d = list(data_pos)
+    train, val = train_test_split(d, test_size=0.3)
+    print (train)
+    print (val)
