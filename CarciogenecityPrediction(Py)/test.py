65d1ead4f52bddbd6404c00d41855cd79731f0bb
DobinFubiss <simonkuerten97@gmail.com>
2021-07-06 10:33:29 +0200

    test

--- CarciogenecityPrediction(Py)/test.py
+++ CarciogenecityPrediction(Py)/test.py
@@ -3,4 +3,6 @@ from TurtleParser import TurtleParser
 if __name__ == "__main__":
     test = TurtleParser()
     test.parse_rdf()
-    print(test.get_labels('https://lpbenchgen.org/resource/lp_4', 1))
+    bla = test.get_subjects().pop()
+    print(bla)
+    print(test.get_labels(bla, 1))
