156024c54e97543b9ab35db7ec494a9a31085833
Susmita Goswami <sg6428397@gmail.com>
2021-07-09 04:26:58 +0200

    Output format converted and generating the expected output for lp_4

--- CarciogenecityPrediction(Py)/algorithm.py
+++ CarciogenecityPrediction(Py)/algorithm.py
@@ -1,6 +1,6 @@
 import math
 import random
-from rdflib import Graph
+from rdflib import Graph, Namespace, Literal, URIRef
 from Ontolearn.ontolearn import KnowledgeBase
 from Ontolearn.ontolearn.concept_learner import CELOE
 
@@ -16,7 +16,7 @@ allNegativeLabels = []
 # Initializing a graph and parsing a turtle file in graph structure
 
 g = Graph()
-result = g.parse("kg-mini-project-train_v2.ttl", format="ttl")
+result = g.parse("kg-mini-project-train_v2_mini.ttl", format="ttl")
 
 for s in g.subjects():
     subjects.add(s)
@@ -90,5 +90,31 @@ test_n = set(test_n)
 model.fit(pos=train_p, neg=train_n)
 hypotheses = model.best_hypotheses(n=3)
 predictions = model.predict(individuals=list(test_p) + list(test_n), hypotheses=hypotheses)
-print(predictions)
+predictions['pred'] = round(predictions.mean(1))
+
+out_g = Graph()
+lpres = Namespace("https://lpbenchgen.org/resource/")
+lpprop = Namespace("https://lpbenchgen.org/property/")
+carcinogenesis= Namespace("http://dl-learner.org/carcinogenesis#")
+out_g.bind("carcinogenesis", carcinogenesis)
+out_g.bind("lpres", lpres)
+out_g.bind("lpprop", lpprop)
+result_1pos = URIRef(lpres.result_1pos)
+result_1neg = URIRef(lpres.result_1neg)
+out_g.add((result_1pos, lpprop.belongsToLP , Literal(True) ))
+out_g.add((result_1pos, lpprop.pertainsTo , lpres.lp_4 ))
+
+out_g.add((result_1neg, lpprop.belongsToLP , Literal(False) ))
+out_g.add((result_1neg, lpprop.pertainsTo , lpres.lp_4 ))
+
+
+for item in predictions.index:
+    pred = predictions.loc[item]['pred']
+    comp = str(item).split('.')[-1]
+    if pred > 0.5:
+        out_g.add((result_1pos, lpprop.resource , carcinogenesis[comp]) )
+    else:
+        out_g.add((result_1neg, lpprop.resource, carcinogenesis[comp]))
+#print(predictions)
+out_g.serialize(destination='prediction_output.ttl', format='turtle')
 print('end')
